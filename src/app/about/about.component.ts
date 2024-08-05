8ac0ae6bcd820f15dd73c744db2c82ef16c1aa65
dariodaddamio <dario.daddamio@gmail.com>
2024-08-05 16:49:20 -0700

    img path sorted + top fixed nav (?) + sticky bot footer

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { AppModule } from '../app.module';
 @Component({
   selector: 'app-about',
   standalone: true,
@@ -8,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-
+  constructor(private router: Router) {}
+  navigateToHome() {
+    this.router.navigate(['/home']);
+  }
+  navigateToGD() {
+    this.router.navigate(['/graphic-design']);
+  }
+  navigateToAbout() {
+    this.router.navigate(['/about']);
+  }
+  navigateToProgramming() {
+    this.router.navigate(['/programming']);
+  }
+  navigateToContact() {
+    this.router.navigate(['/contact']);
+  }
+  getYear(): number {
+    return AppModule.getYear();
+  }
 }
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { AppModule } from '../app.module';
 @Component({
   selector: 'app-contact',
   standalone: true,
@@ -8,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-
+  constructor(private router: Router) {}
+  navigateToHome() {
+    this.router.navigate(['/home']);
+  }
+  navigateToGD() {
+    this.router.navigate(['/graphic-design']);
+  }
+  navigateToAbout() {
+    this.router.navigate(['/about']);
+  }
+  navigateToProgramming() {
+    this.router.navigate(['/programming']);
+  }
+  navigateToContact() {
+    this.router.navigate(['/contact']);
+  }
+  getYear(): number {
+    return AppModule.getYear();
+  }
 }
